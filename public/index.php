8355aac69dab3b85c71f0c93365020970f1fa988
LAINES <LAINES@LAINES-PC>
2017-01-30 17:33:13 -0500

    Terminado

--- public/index.php
+++ public/index.php
@@ -6,19 +6,12 @@ require '../vendor/autoload.php';
 
 $app = new Slim\App();
 
-// Get container
 $container = $app->getContainer();
 
-// Register component on container
 $container['view'] = function ($container) {
     return new \Slim\Views\PhpRenderer('../templates');
 };
 
-/*$app->get('/home', function ($request, $response, $args) {
-    return $this->view->render($response, 'list.html', [
-        'name' => $args['name']
-    ]);
-})->setName('profile');*/
 $app->get('/', function (Request $request, Response $response) {
     
     $obj = new Datos();
